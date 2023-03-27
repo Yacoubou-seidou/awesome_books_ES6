c3fa31b6b16791b9a393a5278873ea0a25b56bbb
yacoubou-seidou <yacoseidou@gmail.com>
2023-03-27 15:52:20 +0100

    Complete js html and css part

---
+++ index.js
@@ -0,0 +1,43 @@
+import addBook from './modules/addBook.js';
+import loadBooks from './modules/loadBooks.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
+
+const addButton = document.querySelector('#add_book');
+const formContainer = document.querySelector('.book-form');
+const bookListSection = document.querySelector('.book-list-section');
+const contactSection = document.querySelector('.contact-section');
+const listLink = document.querySelector('#list-btn');
+const AddNewLink = document.querySelector('#add-btn');
+const contactLink = document.querySelector('#contact-btn');
+const bookNameInput = document.querySelector('#book-name');
+const bookAuthorInput = document.querySelector('#book-author');
+const now = DateTime.now();
+const todayTime = now.toJSDate().toGMTString();
+let bookList = [];
+const localData = localStorage.getItem('books');
+if (localData) {
+  bookList = JSON.parse(localData);
+}
+
+addButton.addEventListener('click', () => addBook(bookNameInput.value, bookAuthorInput.value, bookList));
+listLink.addEventListener('click', () => {
+  bookListSection.style.display = 'block';
+  formContainer.style.display = 'none';
+  contactSection.style.display = 'none';
+});
+
+AddNewLink.addEventListener('click', () => {
+  bookListSection.style.display = 'none';
+  formContainer.style.display = 'flex';
+  contactSection.style.display = 'none';
+});
+
+contactLink.addEventListener('click', () => {
+  bookListSection.style.display = 'none';
+  contactSection.style.display = 'flex';
+  formContainer.style.display = 'none';
+});
+
+const dateContainer = document.querySelector('.date-container');
+dateContainer.innerHTML = todayTime;
+loadBooks(bookList);
